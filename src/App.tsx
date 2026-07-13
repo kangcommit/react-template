db6d217e0ebed3afb60b56bed85b8b3e9834a4ab
kangcommit <ahmad.codespace@gmail.com>
2026-07-13 09:01:55 +0700

    chore: set up routing with BrowserRouter and Routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes } from "react-router";
+
 function App() {
-	return <h1>Hello</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* TODO: Define your application's routes using <Route> components */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
